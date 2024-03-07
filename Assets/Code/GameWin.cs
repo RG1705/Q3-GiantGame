5f19b40e6d4db19761accaebe153d36377d0648e
Gefter <1713331@mukilteo.wednet.edu>
2024-03-07 09:01:59 -0800

    GAME IS DONE WE NEED VISUAL STUFF

---
+++ Assets/Code/GameWin.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameWin : MonoBehaviour
+{
+    public GameObject Castlefab;
+    public GameObject Castlefab2;
+    public GameObject Castle1;
+    public GameObject Castle2;
+    public GameObject Ball;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Castle2 == null)
+        {
+            Debug.Log("Player 1 wins!");
+
+
+        }
+        if (Castle1 == null)
+        {
+            Debug.Log("Player 2 wins!");
+
+
+        }
+
+        if(Castle1 == null || Castle2 == null)
+        {
+            Ball.GetComponent<BallChecker>().enabled = false;
+
+
+
+        }
+
+        if (Input.GetKey(KeyCode.R) && Castle1 == null)
+        {
+
+            Ball.GetComponent<BallChecker>().enabled = true;
+            Instantiate(Castlefab, new Vector2(-10.0f, 0.0f), Quaternion.identity);
+            Castle1 = GameObject.Find("Castle(Clone)");
+            
+
+
+        } else if (Input.GetKey(KeyCode.R)&& Castle2 == null)
+        {
+
+            
+
+                Ball.GetComponent<BallChecker>().enabled = true;
+                Instantiate(Castlefab2, new Vector2(10.0f, 0.0f), Quaternion.identity);
+            Castle2 = GameObject.Find("Castle2(Clone)");
+
+
+
+            
+
+
+        }
+    }
+}
